de1002cb35ded0efcbeaecb6a13c9673859d9312
Pamela Jane Viloria <pamelajane.viloria@gmail.com>
2025-02-06 20:35:42 +0800

    fix: responsiveness and transform

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
 	
 	return (
 		<div className="gradient-container reltative w-screen h-screen overflow-hidden flex flex-row justify-center items-center">
-			<div className={`left-panel xl:size-[800px] lg:size-[500px] md:size-[400px] size-[200px] ${ isFlipped ? "animate-flip-gradient" : "" }`}></div>
-			<div className={`right-panel xl:size-[800px] lg:size-[500px] md:size-[400px] size-[200px] ${ isFlipped ? "animate-flip-gradient" : "" }`}></div>
+			<div className={`left-panel w-full xl:h-[400px] lg:h-[260px] md:h-[200px] h-[110px] ${ isFlipped ? "animate-flip-gradient" : "" }`}></div>
+			<div className={`right-panel w-full xl:h-[400px] lg:h-[260px] md:h-[200px] h-[110px] ${ isFlipped ? "animate-flip-gradient" : "" }`}></div>
     	</div>
 	)
 }
